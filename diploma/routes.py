d986d8df79ceccd5cef654d11db794e75ec23ab8
besmertn <besmertn@gmail.com>
2019-01-27 12:45:27 +0200

    Add base login form

--- diploma/routes.py
+++ diploma/routes.py
@@ -1,6 +1,9 @@
-from diploma import app
 import os
-from flask import send_from_directory
+
+from flask import send_from_directory, render_template, flash, redirect
+
+from diploma import app
+from diploma.forms import LoginForm
 
 
 @app.route('/favicon.ico')
@@ -12,4 +15,15 @@ def favicon():
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Aleksandr'}
+    return render_template('index.html', title='Home', user=user)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html', title='Sign In', form=form)

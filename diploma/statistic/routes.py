f9845caa49fe4adb3d2cacb2f74d12e7e11098d6
Aleksandr Bessmertnyi <Aleksandr.Bessmertnyi@parexel.com>
2019-04-30 11:30:27 +0300

    Add basic line chart to the statistic component

--- diploma/statistic/routes.py
+++ diploma/statistic/routes.py
@@ -1,2 +1,20 @@
+from flask import render_template
+from flask_login import login_required
+
+from diploma.statistic import bp
 
 
+@bp.route('/')
+@login_required
+def index():
+    data = [
+        ['Day', 'PM10', 'PM2.5'],
+        [1, 10, 25],
+        [2, 12, 40],
+        [3, 25, 39],
+        [4, 13, 35],
+        [5, 17, 36],
+        [6, 11, 41],
+        [7, 10, 20]
+    ]
+    return render_template('statistic/index.html', title='Statistic', data=data)
